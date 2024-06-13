9733139226344d6bf44dd72c2e113e3875749b02
weisje <20269574+weisje@users.noreply.github.com>
2024-06-13 06:42:31 -0400

    Created main() function. Defined board dimensions. Randomly applied cell states to the board & stored them as list of lists.  Printed initial board state to screen.

--- conway.py
+++ conway.py
@@ -1,1 +1,41 @@
-print("Success")
+# IMPORT BLOCK BEGIN
+import copy
+import random
+import time
+
+# IMPORT BLOCK END
+
+
+# FUNCTION BLOCK BEGIN
+def main():
+    WIDTH = 60
+    HEIGHT = 20
+
+    # Create a list of lists for the cells
+    nextCells = []
+
+    for xCell in range(WIDTH):
+        column = []
+        for yCell in range(HEIGHT):
+            if random.randint(0, 1) == 0:
+                column.append('#') # Add a living cell to the column list
+            else:
+                column.append(' ') # Add a dead cell
+        nextCells.append(column) # Append the cells to the board after they have been populated
+
+    while True:
+        print("\n\n\n\n\n") # Separate each step with newlines
+        currentCells = copy.deepcopy(nextCells) # Grab the current board set as a new, separate instance
+
+        # Print the current cells on screen
+        for yCoord in range(HEIGHT):
+            for xCoord in range(WIDTH):
+                print(currentCells[xCoord][yCoord], end="")
+            print()
+
+
+# FUNCTION BLOCK END
+
+
+if __name__ == '__main__':
+    main()
